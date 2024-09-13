b8e982ae8260b6e0d1c1ffa3c6f58e0f8f182ea6
SharpDevOps10 <td090833@gmail.com>
2024-09-13 23:03:34 +0300

    Add the second lab

---
+++ Lab2/src/main/java/com/daniorerio/Person.java
@@ -0,0 +1,52 @@
+package com.daniorerio;
+
+import java.util.Objects;
+
+public final class Person {
+    private final String firstName;
+    private final String lastName;
+    private final Integer age;
+
+    public Person(String firstName, String lastName, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Person person = (Person) obj;
+        return Objects.equals(age, person.getAge()) &&
+                Objects.equals(lastName, person.getLastName()) &&
+                Objects.equals(firstName, person.getFirstName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
