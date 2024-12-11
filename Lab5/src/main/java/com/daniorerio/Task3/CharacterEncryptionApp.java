b874ed926a79e6ea9326aa3d7f31f5c136536397
SharpDevOps10 <td090833@gmail.com>
2024-12-11 23:09:46 +0200

    lab10: added the third task

--- Lab5/src/main/java/com/daniorerio/Task3/CharacterEncryptionApp.java
+++ Lab5/src/main/java/com/daniorerio/Task3/CharacterEncryptionApp.java
@@ -1,7 +1,9 @@
 package com.daniorerio.Task3;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,47 +13,79 @@ public class CharacterEncryptionApp {
     private static final Logger logger = LogManager.getLogger(CharacterEncryptionApp.class);
 
     public static void main(String[] args) {
-        Configurator.setRootLevel(Level.INFO);
-        try (Scanner scanner = new Scanner(System.in)) {
-            logger.info("Запуск програми шифрування/розшифрування.");
+        Configurator.setRootLevel(Level.DEBUG);
 
-            System.out.print("Введіть шлях до вхідного файлу: ");
-            String inputFilePath = scanner.nextLine();
-            System.out.print("Введіть шлях до файлу для збереження зашифрованих даних: ");
-            String encryptedFilePath = scanner.nextLine();
-            System.out.print("Введіть шлях до файлу для збереження розшифрованих даних: ");
-            String decryptedFilePath = scanner.nextLine();
-            System.out.print("Введіть ключовий символ для шифрування: ");
-            char keyChar = scanner.next().charAt(0);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            Locale locale = selectLanguage(reader);
+            ResourceBundle messages = ResourceBundle.getBundle("location.messages", locale);
+            logger.info(messages.getString("start"));
+
+            System.out.print(messages.getString("enterInputFilePath"));
+            String inputFilePath = reader.readLine();
+
+            System.out.print(messages.getString("enterEncryptedFilePath"));
+            String encryptedFilePath = reader.readLine();
+
+            System.out.print(messages.getString("enterDecryptedFilePath"));
+            String decryptedFilePath = reader.readLine();
+
+            System.out.print(messages.getString("key"));
+            String keyInput = reader.readLine();
+            char keyChar = keyInput.charAt(0);
 
             try (
                     FileInputStream inputFileStream = new FileInputStream(inputFilePath);
                     FileOutputStream encryptedFileStream = new FileOutputStream(encryptedFilePath);
                     FileOutputStream decryptedFileStream = new FileOutputStream(decryptedFilePath)
             ) {
-                logger.info("Шифрування файлу: {}", inputFilePath);
-                try (Encryptor encryptor = new Encryptor(encryptedFileStream, keyChar)) {
+                logger.info("{} {}", messages.getString("encrypting"), inputFilePath);
+                try (Encryptor encryptor = new Encryptor(encryptedFileStream, keyChar, messages)) {
                     int data;
                     while ((data = inputFileStream.read()) != -1) {
                         encryptor.write(data);
                     }
                 }
-                logger.info("Шифрування завершено, збережено в {}", encryptedFilePath);
+                logger.info("{} {}", messages.getString("encryptionComplete"), encryptedFilePath);
 
-                logger.info("Розшифрування файлу: {}", encryptedFilePath);
-                try (Decryptor decryptor = new Decryptor(new FileInputStream(encryptedFilePath), keyChar)) {
+                logger.info("{} {}", messages.getString("decrypting"), encryptedFilePath);
+                try (Decryptor decryptor = new Decryptor(new FileInputStream(encryptedFilePath), keyChar, messages)) {
                     int data;
                     while ((data = decryptor.read()) != -1) {
-                         decryptedFileStream.write(data);
+                        decryptedFileStream.write(data);
                     }
                 }
-                logger.info("Розшифрування завершено, збережено в {}", decryptedFilePath);
+                logger.info("{} {}", messages.getString("decryptionComplete"), decryptedFilePath);
 
-                logger.info("Файл зашифровано і розшифровано успішно.");
-
+                logger.info(messages.getString("success"));
             } catch (IOException e) {
-                logger.fatal("Помилка при обробці файлу: {}", e.getMessage());
+                logger.fatal("{} {}", messages.getString("fileFat"), e.getMessage());
             }
+        } catch (IOException e) {
+            logger.error("Error reading input: {}", e.getMessage());
+        }
+    }
+
+    private static Locale selectLanguage(BufferedReader reader) {
+        try {
+            System.out.println("Select language: ");
+            System.out.println("1 - English");
+            System.out.println("2 - German");
+            System.out.println("3 - Norwegian");
+
+            int languageChoice = Integer.parseInt(reader.readLine());
+
+            return switch (languageChoice) {
+                case 1 -> Locale.ENGLISH;
+                case 2 -> Locale.GERMANY;
+                case 3 -> Locale.forLanguageTag("no-NO");
+                default -> {
+                    logger.warn("Invalid language choice. Default language is English.");
+                    yield Locale.ENGLISH;
+                }
+            };
+        } catch (Exception e) {
+            logger.error("Error selecting language: {}", e.getMessage());
+            return Locale.ENGLISH;
         }
     }
 }
--- Lab5/src/main/java/com/daniorerio/Task3/Decryptor.java
+++ Lab5/src/main/java/com/daniorerio/Task3/Decryptor.java
@@ -3,6 +3,7 @@ package com.daniorerio.Task3;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ResourceBundle;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,16 +11,19 @@ import org.apache.logging.log4j.Logger;
 public class Decryptor extends FilterInputStream {
     private final int keyOffset;
     private static final Logger logger = LogManager.getLogger(Decryptor.class);
-    public Decryptor(InputStream in, char keyChar) {
+    private final ResourceBundle messages;
+
+    public Decryptor(InputStream in, char keyChar, ResourceBundle messages) {
         super(in);
         this.keyOffset = keyChar;
+        this.messages = messages;
     }
 
     @Override
     public int read() throws IOException {
         int b = super.read();
         if (b == -1) return -1;
-        logger.debug("Розшифрування байта: {}", b);
+        logger.debug("{}: {}", messages.getString("decryptingByte"), b);
         return b - keyOffset;
     }
 }
